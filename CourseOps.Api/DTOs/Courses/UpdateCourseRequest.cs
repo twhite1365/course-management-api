49e8736ace44a5b6b037a281bf7b592276aa6bad
twhite1365 <twhite1365@comcast.net>
2026-02-26 21:32:03 -0500

    feat(api): add UpdateCourseRequest DTO for course updates

---
+++ CourseOps.Api/DTOs/Courses/UpdateCourseRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseOps.Api.DTOs.Courses
+{
+    public class UpdateCourseRequest
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public byte[] RowVersion { get; set; } = Array.Empty<byte>();
+    }
+}
